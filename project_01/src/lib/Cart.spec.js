4a4a184fc54ef4108d2b4f85757a0be77f448710
Igor Melo <igorjmelo4@gmail.com>
2021-01-14 23:38:21 -0300

    shopping cart

---
+++ project_01/src/lib/Cart.spec.js
@@ -0,0 +1,64 @@
+import Cart from './Cart';
+
+describe('Cart', () => {
+  let cart;
+
+  let product = {
+    title: 'Adidas Running shoes - men',
+    price: 35388,
+  };
+
+  let product2 = {
+    title: 'Adidas Running shoes - women',
+    price: 41872,
+  };
+
+  beforeEach(() => {
+    cart = new Cart();
+  });
+
+  it('should return 0 when getTotal() is executed in a newly created instance', () => {
+    expect(cart.getTotal()).toEqual(0);
+  });
+
+  it('should multiply quantity and price and receive the total amount', () => {
+    const item = {
+      product,
+      quantity: 2,
+    };
+
+    cart.add(item);
+
+    expect(cart.getTotal()).toEqual(70776);
+  });
+
+  it('should ensure no more than one product exists at a time', () => {
+    cart.add({
+      product,
+      quantity: 2,
+    });
+
+    cart.add({
+      product,
+      quantity: 1,
+    });
+
+    expect(cart.getTotal()).toEqual(35388);
+  });
+
+  it('should update total when a product gets included and then remove', () => {
+    cart.add({
+      product,
+      quantity: 2,
+    });
+
+    cart.add({
+      product: product2,
+      quantity: 1,
+    });
+
+    cart.remove(product);
+
+    expect(cart.getTotal()).toEqual(41872);
+  });
+});
